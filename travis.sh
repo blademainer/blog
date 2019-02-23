4bb8a8b40a2748e8e91fbbfcb5805310a831511e
blademainer <blademainer@gmail.com>
2019-02-24 02:22:15 +0800

    fix travis

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 args="$1"
-docker run --rm -it -v $(pwd):/project skandyla/travis-cli $args
+docker run --rm -it -v $(pwd):/project -v $(pwd)/.travis:/root/.travis skandyla/travis-cli $args
 
---
+++ travis_ci.sh
@@ -0,0 +1,1 @@
+docker run --rm --entrypoint= -it -v /Users/xiongyingqi/workspace/blog/blog:/project skandyla/travis-cli /bin/sh
