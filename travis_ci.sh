ddbf30fbdf0824c2c68f704171effa88cb090144
blademainer <blademainer@gmail.com>
2019-03-07 01:06:48 +0800

    update

--- travis_ci.sh
+++ travis_ci.sh
@@ -1,1 +1,1 @@
-docker run --rm --entrypoint= -it -v /Users/xiongyingqi/workspace/blog/blog:/project skandyla/travis-cli /bin/sh
+docker run --rm --entrypoint= -it -v `pwd`:/project skandyla/travis-cli /bin/sh
