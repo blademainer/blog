8ac54fc44b7802288fe2a3740d4c2da94a9d9613
blademainer <blademainer@gmail.com>
2019-03-08 00:00:52 +0800

    deploy by hexo-admin

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config user.email "blademainer@gmail.com"
 git config user.name "blademainer"
-git status && git add ./ -A && git commit -m "deploy by hexo-admin" && git push
+git status && git add ./ -A && git commit -m "deploy by hexo-admin" && git pull && git push
