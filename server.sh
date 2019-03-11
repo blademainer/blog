8dc8d7ea4bc5b15281f72019ca106c18a168795b
blademainer <blademainer@gmail.com>
2019-03-12 00:37:22 +0800

    update server.sh

--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 4000:4000 -p 3000:3000 -it -w /app -v ~/.ssh:/root/.ssh -v `pwd`:/app blademainer/hexo:v1.0.2 hexo server
+docker run --rm -p 4000:4000 -p 3000:3000 -p 3001:3001 -it -w /app -v ~/.ssh:/root/.ssh -v `pwd`:/app blademainer/hexo:v1.0.2 hexo server
